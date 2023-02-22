60e670c6c5b586523241955825312f1b907f5012
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-22 16:32:43 -0600

    Adding resolvers, typeDefs and index files for Schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Query {
+        users: [User]
+        user(username: String!): User
+        me: User
+    }
+
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: String!): User
+    }
+
+
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+module.exports = typeDefs;
